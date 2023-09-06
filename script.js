3f145937110486fb3c4cb388cec8d7b51341734f
Radu-A <elcastravete@gmail.com>
2023-09-06 22:09:19 +0200

    finished/desktop_view

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuDiv = document.querySelector(".menu-div");
+const openLink = document.querySelector(".open-link");
+const closeLink = document.querySelector(".close-link");
+
+openLink.addEventListener("click", () => {
+  menuDiv.classList.add("active");
+});
+
+closeLink.addEventListener("click", () => {
+  menuDiv.classList.remove("active");
+});
