81b1512b04369e9b843d1548be082249bbeec474
Radu-A <elcastravete@gmail.com>
2023-09-07 18:02:47 +0200

    fixed/screen_on_menu

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const menuDiv = document.querySelector(".menu-div"),
   openLink = document.querySelector(".open-link"),
   closeLink = document.querySelector(".close-link");
+layer = document.querySelector(".layer");
 
 openLink.addEventListener("click", () => {
   menuDiv.classList.add("active");
+  layer.classList.add("active");
+  document.body.classList.add("fixed");
 });
 
 closeLink.addEventListener("click", () => {
   menuDiv.classList.remove("active");
+  layer.classList.remove("active");
+  document.body.classList.remove("fixed");
 });
