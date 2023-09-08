776676bd5642f79a88fbbe3044e8f161c0ca5305
Radu-A <elcastravete@gmail.com>
2023-09-08 11:34:12 +0200

    added/css_transitions

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const menuDiv = document.querySelector(".menu-div"),
   openLink = document.querySelector(".open-link"),
-  closeLink = document.querySelector(".close-link");
+  closeLink = document.querySelector(".close-link"),
+  closeDiv = document.querySelector(".icon-close-div");
 layer = document.querySelector(".layer");
 
+// openLink.addEventListener("click", () => {
+//   menuDiv.classList.add("active");
+//   layer.classList.add("active");
+//   document.body.classList.add("fixed");
+// });
+
+// closeLink.addEventListener("click", () => {
+//   menuDiv.classList.remove("active");
+//   layer.classList.remove("active");
+//   document.body.classList.remove("fixed");
+// });
 openLink.addEventListener("click", () => {
-  menuDiv.classList.add("active");
+  menuDiv.style.width = '69%';
+  closeDiv.style.display = 'block';
   layer.classList.add("active");
   document.body.classList.add("fixed");
 });
 
 closeLink.addEventListener("click", () => {
-  menuDiv.classList.remove("active");
+  menuDiv.style.width = '0%';
+  closeDiv.style.display = 'none';
   layer.classList.remove("active");
   document.body.classList.remove("fixed");
 });
